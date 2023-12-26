ee04f058e216ae706eda2b562405597d7ccf5ca4
Datou0718 <lyck92@gmail.com>
2023-12-26 15:25:55 +0800

    fix merge

--- llama/evaluate/eval.sh
+++ llama/evaluate/eval.sh
@@ -1,14 +1,7 @@
-model=""
-data=""
-if [[ $1 == "llama" ]]; then
-    model="../llama/Taiwan-LLM-13B-v2.0-chat"
-    data="../data/preprocessed/traditional/gpt_alpaca_test.json"
-elif [[ $1 == "mistral" ]]; then
-    model="./mistral/Mistral-7B-v0.1-chinese"
-    data="./data/preprocessed/simplified/gpt_alpaca_test.json"
-fi
+model="../llama/Taiwan-LLM-13B-v2.0-chat"
+data="../data/preprocessed/mix_dataset_test.json"
 
 python3 ppl.py \
   --base_model_path "$model" \
-  --peft_path "$2" \
+  --peft_path "$1" \
   --test_data_path "$data"
